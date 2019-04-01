9146bc3b65809325b1207ee481e5c1507dbe72ae
Tobias Burdow [Kaleidox] <42473485+burdoto@users.noreply.github.com>
2019-04-01 10:52:48 +0200

    Added JavaDoc to interfaces

--- src/main/java/de/kaleidox/util/interfaces/Terminatable.java
+++ src/main/java/de/kaleidox/util/interfaces/Terminatable.java
@@ -2,6 +2,15 @@ package de.kaleidox.util.interfaces;
 
 import java.io.IOException;
 
+/**
+ * Defines an object to require termination.
+ * During termination, the class may throw IOExceptions.
+ */
 public interface Terminatable {
+    /**
+     * The method to be called to terminate the object.
+     *
+     * @throws IOException Exceptions that happen during IO operations on termination.
+     */
     void terminate() throws IOException;
 }
