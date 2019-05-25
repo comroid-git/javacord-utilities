048e91ee3319f4f3cb6342bae709674e17f45f38
burdoto <burdoto@outlook.com>
2019-05-25 09:26:34 +0200

    Fix ServerPropertiesManagerTest to use proper methods for a general use-case

--- src/test/java/de/kaleidox/javacord/util/test/server/properties/ServerPropertiesManagerTest.java
+++ src/test/java/de/kaleidox/javacord/util/test/server/properties/ServerPropertiesManagerTest.java
@@ -46,21 +46,21 @@ public class ServerPropertiesManagerTest {
     public void testSerialization() throws IOException {
         ServerPropertiesManager deserializer = new ServerPropertiesManager(propertiesFile);
 
-        PropertyGroup traitsProperty = deserializer.getProperty("bot.traits");
+        PropertyGroup traitsProperty = deserializer.register("bot.traits", 419);
         assertEquals(420, traitsProperty.getValue(100).asInt());
         assertEquals(419, traitsProperty.getValue(200).asInt());
         assertEquals(419, traitsProperty.getDefaultValue().asInt());
         assertEquals("Traits", traitsProperty.getDisplayName());
         assertEquals("These are traits", traitsProperty.getDescription());
 
-        PropertyGroup nameProperty = deserializer.getProperty("bot.name");
+        PropertyGroup nameProperty = deserializer.register("bot.name", "William");
         assertEquals("Alfred", nameProperty.getValue(100).asString());
         assertEquals("William", nameProperty.getValue(200).asString());
         assertEquals("William", nameProperty.getDefaultValue().asString());
         assertEquals("Name", nameProperty.getDisplayName());
         assertEquals("These are names", nameProperty.getDescription());
 
-        PropertyGroup emojiProperty = deserializer.getProperty("bot.emoji");
+        PropertyGroup emojiProperty = deserializer.register("bot.emoji", "\uD83C\uDF61");
         assertEquals("\uD83D\uDD12", emojiProperty.getValue(100).asString());
         assertEquals("\uD83C\uDF61", emojiProperty.getValue(200).asString());
         assertEquals("\uD83C\uDF61", emojiProperty.getDefaultValue().asString());
