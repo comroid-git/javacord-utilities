29263d718bdd5d7f818078c4d21512875cb0daa1
burdoto <burdoto@outlook.com>
2019-05-27 13:47:00 +0200

    Parse stringified arguments as single strings

--- src/main/java/de/kaleidox/javacord/util/commands/CommandHandler.java
+++ src/main/java/de/kaleidox/javacord/util/commands/CommandHandler.java
@@ -334,7 +334,7 @@ public final class CommandHandler {
         if (usedPrefix == -1 || pref == null) return;
 
         CommandRepresentation cmd;
-        String[] split = content.split("[\\s&&[^\\n]]++");
+        String[] split = splitContent(content);
         String[] args;
         if (pref[usedPrefix].matches("^(.*\\s.*)+$")) {
             cmd = commands.get(split[1]);
@@ -408,6 +408,88 @@ public final class CommandHandler {
                 .getExecutorService()
                 .submit(() -> doInvoke(cmd, commandParams, channel, message));
         else doInvoke(cmd, commandParams, channel, message);
+    }
+
+    private String[] splitContent(String content) {
+        List<String> yields = new ArrayList<>();
+        yields.add("");
+
+        boolean inString = false;
+        int i = 0, y = 0, s = -1;
+        char c = 0, p;
+
+        while (i < content.length()) {
+            p = c;
+            c = content.charAt(i);
+
+            switch (c) {
+                case '"':
+                    // if not in string & starter is not escaped
+                    if (!inString && p != '\\') {
+                        // if string starts after a space
+                        if (p == ' ') {
+                            // start string
+                            yields.add("");
+                            s = y++;
+                            inString = true;
+                        } else {
+                            // escape "
+                            yields.set(y, yields.get(y) + c);
+                        }
+                        // if in string & ender is not escaped
+                    } else if (inString && p != '\\') {
+                        // if next char is space
+                        if (content.charAt(i + 1) == ' ') {
+                            // end string
+                            yields.add("");
+                            s = y++;
+                            inString = false;
+                        } else {
+                            // escape "
+                            yields.set(y, yields.get(y) + c);
+                        }
+                        // if " was escaped
+                    } else {
+                        // append to string
+                        yields.set(y, yields.get(y) + c);
+                    }
+                    break;
+                case ' ':
+                    if (inString) {
+                        // append to string
+                        yields.set(y, yields.get(y) + c);
+                    } else {
+                        // start new item
+                        yields.add("");
+                        s = y++;
+                    }
+                    break;
+                case '\\':
+                    // never include escaping character
+                    break;
+                default:
+                    // append to string
+                    yields.set(y, yields.get(y) + c);
+                    break;
+            }
+
+            i++;
+        }
+
+        if (inString) { // if still in string -> join split string together
+            int prevSize = yields.size();
+            new ArrayList<>(yields)
+                    .stream()
+                    .skip(s)
+                    .flatMap(str -> Arrays.stream(str.split(" ")))
+                    .forEachOrdered(yields::add);
+            if (prevSize >= s + 1) yields.subList(s + 1, prevSize + 1).clear();
+            yields.set(s + 1, '"' + yields.get(s + 1));
+        }
+
+        yields.removeIf(String::isEmpty);
+
+        return yields.toArray(new String[0]);
     }
 
     private void doInvoke(CommandRepresentation commandRep, Params commandParams, TextChannel channel, Message message) {
