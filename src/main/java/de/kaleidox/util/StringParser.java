dce8151105b200c696027360fde44b7dd4545af7
burdoto <burdoto@outlook.com>
2019-07-08 22:37:06 +0200

    Update StringParser.java

--- src/main/java/de/kaleidox/util/StringParser.java
+++ src/main/java/de/kaleidox/util/StringParser.java
@@ -35,92 +35,112 @@ public enum StringParser {
      * @return
      */
     public String[] parseStrings(int option, String delimiter, String... args) {
-        String content = String.join(delimiter, args);
+        // todo rewrite
 
-        List<String> yields = new ArrayList<>();
-        yields.add("");
+        // if OPTION is lower than -2, it is bad argument
+        if (option < -2)
+            throw new IllegalArgumentException("Option cannot be lower than -2");
 
-        boolean inString = false;
-        int i = 0, y = 0, s = -1;
-        char c = 0, p;
+        // if OPTION is -2, string parsing is disabled
+        if (option == -2)
+            return args;
 
-        while (i < content.length()) {
-            p = c;
-            c = content.charAt(i);
+        if (option == -1) {
+            // default string identifying; processing " chars
 
-            switch (c) {
-                case '"':
-                    // if not in string & starter is not escaped
-                    if (!inString && p != '\\') {
-                        // if string starts after a space
-                        if (p == ' ') {
-                            // start string
-                            yields.add("");
-                            s = y++;
-                            inString = true;
-                        } else {
-                            // escape "
-                            yields.set(y, yields.get(y) + c);
-                        }
-                        // if in string & ender is not escaped
-                    } else if (inString && p != '\\') {
-                        // if there are more chars
-                        if (content.length() < i + 1) {
-                            // if next char is space
-                            if (content.length() < i + 1 && content.charAt(i + 1) == ' ') {
-                                // end string
+
+            // old code
+            String content = String.join(delimiter, args);
+
+            List<String> yields = new ArrayList<>();
+            yields.add("");
+
+            boolean inString = false;
+            int i = 0, y = 0, s = -1;
+            char c = 0, p;
+
+            while (i < content.length()) {
+                p = c;
+                c = content.charAt(i);
+
+                switch (c) {
+                    case '"':
+                        // if not in string & starter is not escaped
+                        if (!inString && p != '\\') {
+                            // if string starts after a space
+                            if (p == ' ') {
+                                // start string
                                 yields.add("");
                                 s = y++;
-                                inString = false;
+                                inString = true;
                             } else {
                                 // escape "
                                 yields.set(y, yields.get(y) + c);
                             }
+                            // if in string & ender is not escaped
+                        } else if (inString && p != '\\') {
+                            // if there are more chars
+                            if (content.length() < i + 1) {
+                                // if next char is space
+                                if (content.length() < i + 1 && content.charAt(i + 1) == ' ') {
+                                    // end string
+                                    yields.add("");
+                                    s = y++;
+                                    inString = false;
+                                } else {
+                                    // escape "
+                                    yields.set(y, yields.get(y) + c);
+                                }
+                            } else {
+                                // end string
+                                inString = false;
+                            }
+                            // if " was escaped
                         } else {
-                            // end string
-                            inString = false;
+                            // append to string
+                            yields.set(y, yields.get(y) + c);
                         }
-                        // if " was escaped
-                    } else {
+                        break;
+                    case ' ':
+                        if (inString) {
+                            // append to string
+                            yields.set(y, yields.get(y) + c);
+                        } else {
+                            // start new item
+                            yields.add("");
+                            s = y++;
+                        }
+                        break;
+                    case '\\':
+                        // never include escaping character
+                        break;
+                    default:
                         // append to string
                         yields.set(y, yields.get(y) + c);
-                    }
-                    break;
-                case ' ':
-                    if (inString) {
-                        // append to string
-                        yields.set(y, yields.get(y) + c);
-                    } else {
-                        // start new item
-                        yields.add("");
-                        s = y++;
-                    }
-                    break;
-                case '\\':
-                    // never include escaping character
-                    break;
-                default:
-                    // append to string
-                    yields.set(y, yields.get(y) + c);
-                    break;
+                        break;
+                }
+
+                i++;
             }
 
-            i++;
+            if (inString) { // if still in string -> join split string together
+                int prevSize = yields.size();
+                new ArrayList<>(yields)
+                        .stream()
+                        .skip(s)
+                        .flatMap(str -> Arrays.stream(str.split(" ")))
+                        .forEachOrdered(yields::add);
+                if (prevSize >= s + 1) yields.subList(s + 1, prevSize + 1).clear();
+                yields.set(s + 1, '"' + yields.get(s + 1));
+            }
+
+            yields.removeIf(String::isEmpty);
+
+            return yields.toArray(new String[0]);
+        } else {
+            // concat items after index OPTION; not processing " chars
+
+            throw new UnsupportedOperationException("Not yet implemented!"); // todo
         }
-
-        if (inString) { // if still in string -> join split string together
-            int prevSize = yields.size();
-            new ArrayList<>(yields)
-                    .stream()
-                    .skip(s)
-                    .flatMap(str -> Arrays.stream(str.split(" ")))
-                    .forEachOrdered(yields::add);
-            if (prevSize >= s + 1) yields.subList(s + 1, prevSize + 1).clear();
-            yields.set(s + 1, '"' + yields.get(s + 1));
-        }
-
-        yields.removeIf(String::isEmpty);
-
-        return yields.toArray(new String[0]);
     }
 }
