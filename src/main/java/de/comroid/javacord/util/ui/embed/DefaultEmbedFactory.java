7fd346f5ce44b64363489eb4fc083c49f375a84a
Kaleidox <burdoto@outlook.com>
2019-11-08 14:19:55 +0100

    Refactor to de.comroid

---
+++ src/main/java/de/comroid/javacord/util/ui/embed/DefaultEmbedFactory.java
@@ -0,0 +1,28 @@
+package de.comroid.javacord.util.ui.embed;
+
+import java.util.function.Supplier;
+
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+
+public enum DefaultEmbedFactory implements Supplier<EmbedBuilder> {
+    INSTANCE;
+
+    private Supplier<EmbedBuilder> embedSupplier;
+
+    DefaultEmbedFactory() {
+        this.embedSupplier = EmbedBuilder::new;
+    }
+
+    @Override
+    public EmbedBuilder get() {
+        return embedSupplier.get().removeAllFields();
+    }
+
+    public static void setEmbedSupplier(Supplier<EmbedBuilder> embedSupplier) {
+        INSTANCE.embedSupplier = embedSupplier;
+    }
+
+    public static EmbedBuilder create() {
+        return INSTANCE.get();
+    }
+}
